f81eaddfee4a1703fd5a9b1f8a44b3251b02ffaf
knoxHuang <821686174@qq.com>
2015-03-11 19:06:54 +0800

    fix fireball-x/game-duang-sheep#3: 添加游戏角色（绵羊）

---
+++ assets/script/Sheep.js
@@ -0,0 +1,83 @@
+/**
+ * Created by knox on 2015/3/11.
+ */
+
+var SheepState = (function (t) {
+    t[t.run = 0] = 'run';
+    t[t.jump = 1] = 'jump';
+    t[t.drop = 2] = 'drop';
+    t[t.down = 3] = 'down';
+    t[t.die = 4] = 'die'
+    return t;
+})({});
+
+var Sheep = Fire.extend(Fire.Component, function () {
+    this.anim = null;
+    this.runAnimState = null;
+    this.jumpAnimState = null;
+    this.dropAnimState = null;
+    this.downAnimState = null;
+    this.dieAnimState = null;
+    this.sheepState = SheepState.run;
+    this.tempSpeed = 0;
+});
+
+Sheep.prop('gravity', 9.8);
+
+Sheep.prop('speed', 300);
+
+Sheep.prop('fLoorCoordinates', -130);
+
+Sheep.prototype.onLoad = function () {
+    this.anim = this.entity.getComponent(Fire.SpriteAnimation);
+    this.runAnimState = this.anim.getAnimState('sheep_run');
+    this.jumpAnimState = this.anim.getAnimState('sheep_jump');
+    this.dropAnimState = this.anim.getAnimState('sheep_drop');
+    this.downAnimState = this.anim.getAnimState('sheep_down');
+    this.dieAnimState = this.anim.getAnimState('sheep_die');
+
+    Fire.Input.on('mouseup', function (event) {
+        this.anim.play(this.jumpAnimState, 0);
+        this.sheepState = SheepState.jump;
+        this.tempSpeed = this.speed;
+    }.bind(this));
+};
+
+Sheep.prototype.lateUpdate = function () {
+
+    if(this.sheepState === SheepState.jump || this.sheepState === SheepState.drop) {
+        this.tempSpeed -= Fire.Time.deltaTime * 100 * this.gravity;
+    }
+
+    switch (this.sheepState) {
+        case SheepState.run:
+            break;
+        case SheepState.jump:
+            this.entity.transform.y += Fire.Time.deltaTime * this.tempSpeed;
+            if (this.tempSpeed < 0) {
+                this.anim.play(this.dropAnimState, 0);
+                this.sheepState = SheepState.drop;
+            }
+            break;
+        case SheepState.drop:
+            this.entity.transform.y += Fire.Time.deltaTime * this.tempSpeed;
+            if (this.entity.transform.y <= this.fLoorCoordinates) {
+                this.entity.transform.y = this.fLoorCoordinates;
+                this.anim.play(this.downAnimState, 0);
+                this.sheepState = SheepState.down;
+            }
+            break;
+        case SheepState.down:
+            if (this.downAnimState && !this.anim.IsPlaying(this.downAnimState.name)) {
+                this.anim.play(this.runAnimState, 0);
+                this.sheepState = SheepState.run;
+            }
+            break;
+        case SheepState.die:
+            break;
+        default:
+            break;
+    }
+};
+
+module.exports = Sheep;
--- assets/script/floor.js
+++ assets/script/floor.js
@@ -2,16 +2,15 @@
  * Created by knox on 2015/3/10.
  */
 
-var Floor = Fire.extend(Fire.Component, function () {
+var Floor = Fire.extend(Fire.Component);
 
-});
+Floor.prop('speed', 300);
 
-Floor.prototype.onLoad = function () {
-};
+Floor.prop('x', -858);
 
 Floor.prototype.update = function () {
-    this.entity.transform.x -= Fire.Time.deltaTime * 100;
-    if (this.entity.transform.x < -900 ) {
+    this.entity.transform.x -= (Fire.Time.deltaTime * this.speed);
+    if (this.entity.transform.x < this.x ) {
         this.entity.transform.x = 0;
     }
 };
